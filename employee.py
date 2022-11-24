311b797ee63ad164cdc5619f610d1f1a4b401e1d
vishal8202 <111052930+vishal8202@users.noreply.github.com>
2022-11-24 15:15:56 +0530

    entry

--- employee.py
+++ employee.py
@@ -29,38 +29,11 @@ while True:
         mydb.commit()
     elif(choice==2):
         print('view student')
-        sql = 'SELECT * FROM `emplyees`'
-        mycursor.execute(sql)
-        result = mycursor.fetchall()
-        for i in result:
-            print(i)
     elif(choice==3):
         print('search a student')
-        empcode = input('enter the empcode of the employee to be searched : ')
-        sql = 'SELECT `id`, `empcode`, `empName`, `Designation`, `Salary`, `companyName`, `phoneNumber`, `EmailId`, `Password` FROM `emplyees` WHERE `empcode`='+empcode
-        mycursor.execute(sql)
-        result = mycursor.fetchall()
-        print(result)
     elif(choice==4):
         print('update the student')
-        ecode = input('enter the employee code for the update : ')
-        name = input('enter the name: ')
-        designation = input('enter the designation: ')
-        salary = input('enter the salary: ')
-        company = input('enter the company name: ')
-        phone = input('enter the phone number: ')
-        mail = input('enter the email id: ')
-        password = input('enter the password: ')
-        sql = "UPDATE `emplyees` SET `empcode`='"+ecode+"',`empName`='"+name+"',`Designation`='"+designation+"',`Salary`='"+salary+"',`companyName`='"+company+"',`phoneNumber`='"+phone+"',`EmailId`='"+mail+"',`Password`='"+password+"' WHERE `empcode`="+ecode
-        mycursor.execute(sql)
-        mydb.commit()
-        print('sucessfully updates !!! ')
     elif(choice==5):
         print('delete the student')
-        emcode = input('enter the employees empcode to be deleting : ')
-        sql = 'DELETE FROM `emplyees` WHERE `empcode`='+emcode
-        mycursor.execute(sql)
-        mydb.commit()
-        print('Deleted successfully !!!')
     elif(choice==6):
         break
